f02d02bf257bfe2d9ce29f8b42dcd6ade2045364
qibiell <kobilbryant5@gmail.com>
2024-09-24 13:22:30 +0700

    repo baru

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>halaman saya</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <h1>hallo  guys!</h1>
+
+    <h3>Owner : {{ $nama }} </h3>
+
+    <img src="img/it.jpg" alt="IT Crew" width="300">
+
+</body>
+</html>
